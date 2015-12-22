931a7a62471a605a72175edf1a001f2feb0d4aa2
Andrii Vasyliev <sol@hiqdev.com>
2015-12-22 14:06:29 +0000

    minor

--- .hidev/commits.md
+++ .hidev/commits.md
@@ -3,6 +3,7 @@ hiqdev/hidev-readme commits history
 
 ## Under development
 
+    - 0f49463 2015-12-22 looks working (sol@hiqdev.com)
 - Added basics
     - da24b41 2015-12-21 inited (sol@hiqdev.com)
 
--- tests/functional/ReadmeTest.php
+++ tests/functional/ReadmeTest.php
@@ -1,19 +1,19 @@
 <?php
 
 /*
- * Task runner, code generator and build tool for easier continuos integration
+ * README plugin for HiDev
  *
- * @link      https://github.com/hiqdev/hidev
- * @package   hidev
+ * @link      https://github.com/hiqdev/hidev-readme
+ * @package   hidev-readme
  * @license   BSD-3-Clause
- * @copyright Copyright (c) 2014-2015, HiQDev (http://hiqdev.com/)
+ * @copyright Copyright (c) 2015, HiQDev (http://hiqdev.com/)
  */
 
 namespace base;
 
 use hidev\tests\functional\Tester;
 
-class ReadmeGoalTest extends \PHPUnit_Framework_TestCase
+class ReadmeTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @var \FunctionalTester
