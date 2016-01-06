1bc87ce31476221cba243058f66510f7e4fc8558
Andrii Vasyliev <sol@hiqdev.com>
2016-01-06 17:11:39 +0000

    changed config structure

--- src/yii2-extraconfig.php
+++ src/yii2-extraconfig.php
@@ -11,21 +11,29 @@
 
 return [
     'components' => [
-        'goals' => [
-            'readme'            => [
+        'config' => [
+            'readme' => [
                 'class'          => 'hidev\readme\goals\ReadmeGoal',
                 'markdownBadges' => [
                     'github.version'          => '[![GitHub version](https://badge.fury.io/gh/{{ config.github.vendor }}%2F{{ config.github.package }}.svg)](https://badge.fury.io/gh/{{ config.github.vendor }}%2F{{ config.github.package }})',
                     'versioneye.dependencies' => '[![Dependency Status](https://www.versioneye.com/php/{{ config.vendor.name }}:{{ config.package.name }}/dev-master/badge.svg)](https://www.versioneye.com/php/{{ config.vendor.name }}:{{ config.package.name }}/dev-master)',
                 ],
             ],
-            'README'            => 'hidev\readme\goals\ReadmeTextGoal',
-            'README.md'         => 'hidev\readme\goals\ReadmeMarkdownGoal',
-            'README.txt'        => 'hidev\readme\goals\ReadmeTextGoal',
-            'README.markdown'   => 'hidev\readme\goals\ReadmeMarkdownGoal',
-        ],
-        'views' => [
-            '@hidev/readme/views',
+            'README' => [
+                'class' => 'hidev\readme\goals\ReadmeTextGoal',
+            ],
+            'README.txt' => [
+                'class' => 'hidev\readme\goals\ReadmeTextGoal',
+            ],
+            'README.md' => [
+                'class' => 'hidev\readme\goals\ReadmeMarkdownGoal',
+            ],
+            'README.markdown' => [
+                'class' => 'hidev\readme\goals\ReadmeMarkdownGoal',
+            ],
+            'views' => [
+                '@hidev/readme/views',
+            ],
         ],
     ],
 ];
