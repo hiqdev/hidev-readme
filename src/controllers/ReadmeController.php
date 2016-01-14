fd08580a1a3726c4d3a344ef246d8de0c563960c
Andrii Vasyliev <sol@hiqdev.com>
2016-01-14 14:38:30 +0000

    still debugging

--- src/controllers/ReadmeController.php
+++ src/controllers/ReadmeController.php
@@ -124,7 +124,7 @@ class ReadmeController extends \hidev\controllers\TemplateController
 
     public function renderBadge($tpl)
     {
-        print "BADGE: '$tpl'\n";
+        error_log( "BADGE: '$tpl'\n" );
         return $this->getTwig()->render($tpl, ['config' => $this->takeConfig()]);
     }
 
