22df7052bc3661cf615ad6217f3123567e355d35
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2017-03-23 16:01:52 +0200

    Added PHPUnit 6 compatibility

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -13,6 +13,17 @@ error_reporting(-1);
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
+if (!class_exists('PHPUnit_Framework_Constraint') && class_exists('PHPUnit\Framework\Constraint\Constraint')) {
+    abstract class PHPUnit_Framework_Constraint extends \PHPUnit\Framework\Constraint\Constraint
+    {
+    }
+}
+if (!class_exists('PHPUnit_Framework_TestCase') && class_exists('PHPUnit\Framework\TestCase')) {
+    abstract class PHPUnit_Framework_TestCase extends \PHPUnit\Framework\TestCase
+    {
+    }
+}
+
 use hiqdev\composer\config\Builder;
 use yii\console\Application;
 
