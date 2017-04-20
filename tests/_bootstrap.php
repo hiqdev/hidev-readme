410b993adf38357f5370bf514ed9c40f715f9d97
Andrii Vasyliev <sol@hiqdev.com>
2017-04-20 04:38:12 +0000

    removed phpunit 5 compatibility from tests/_bootstrap

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -13,18 +13,6 @@ error_reporting(-1);
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
-if (!class_exists('PHPUnit_Framework_Constraint') && class_exists('PHPUnit\Framework\Constraint\Constraint')) {
-    abstract class PHPUnit_Framework_Constraint extends \PHPUnit\Framework\Constraint\Constraint
-    {
-    }
-}
-if (!class_exists('PHPUnit_Framework_TestCase') && class_exists('PHPUnit\Framework\TestCase')) {
-    abstract class PHPUnit_Framework_TestCase extends \PHPUnit\Framework\TestCase
-    {
-    }
-}
+$config = require \hiqdev\composer\config\Builder::path('tests');
 
-use hiqdev\composer\config\Builder;
-use yii\console\Application;
-
-Yii::$app = new Application(require Builder::path('tests'));
+Yii::$app = new \yii\console\Application($config);
